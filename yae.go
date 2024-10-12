ecbdca1193e963d91a097beb87821018bf989e4b
Fuwn <contact@fuwn.me>
2024-10-12 00:32:09 -0700

    refactor(utilities): move shared utilities to separate file

--- yae.go
+++ yae.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"os/exec"
 	"strings"
 	"time"
 
@@ -253,39 +252,4 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-}
-
-func fetchSHA256(url string, unpack bool, show bool) (string, error) {
-	arguments := []string{"--type", "sha256", url}
-
-	if unpack {
-		arguments = append([]string{"--unpack"}, arguments...)
-	}
-
-	output, err := command("nix-prefetch-url", show, arguments...)
-
-	if err != nil {
-		return "", err
-	}
-
-	lines := strings.Split(output, "\n")
-
-	return strings.Trim(lines[len(lines)-2], "\n"), nil
-}
-
-func command(name string, show bool, args ...string) (string, error) {
-	executable, err := exec.LookPath(name)
-	out := []byte{}
-
-	if show {
-		cmd := exec.Command(executable, args...)
-		cmd.Stdin = os.Stdin
-		cmd.Stderr = os.Stderr
-		out, err = cmd.Output()
-	} else {
-		cmd := exec.Command(executable, args...)
-		out, err = cmd.Output()
-	}
-
-	return string(out), err
 }
