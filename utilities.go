a01d4a82e323ecbfa8f3a4593e31fce96f76d110
Fuwn <contact@fuwn.me>
2024-10-12 05:11:53 -0700

    refactor(utilities): move lister to utilities

--- utilities.go
+++ utilities.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 	"strings"
@@ -39,4 +40,16 @@ func command(name string, show bool, args ...string) (string, error) {
 	}
 
 	return string(out), err
+}
+
+func lister(items []string) string {
+	if len(items) == 0 {
+		return ""
+	} else if len(items) == 1 {
+		return items[0]
+	} else if len(items) == 2 {
+		return fmt.Sprintf("%s & %s", items[0], items[1])
+	}
+
+	return fmt.Sprintf("%s, & %s", strings.Join(items[:len(items)-1], ", "), items[len(items)-1])
 }
