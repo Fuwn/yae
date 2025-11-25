9e47867a809338c186e5783b4197dc21c43d7a7f
Fuwn <contact@fuwn.me>
2025-11-24 22:10:58 -0800

    chore(flake): Update version

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@
               inherit meta;
 
               pname = name;
-              version = "2025.06.22";
+              version = "2025.11.25";
               src = pkgs.lib.cleanSource ./.;
               vendorHash = "sha256-XQEB2vgiztbtLnc7BR4WTouPI+2NDQXXFUNidqmvbac=";
               buildInputs = if isDarwin then [ ] else [ pkgs.musl ];
